d27370d561d5a6c20d3ede5f343c83db1ee0155a
esayadmohamed <esayadmohamed@gmail.com>
2025-04-30 20:14:06 +0100

    Made changes to my app

--- initdb.js
+++
@@ -1,9 +0,0 @@
-// const sql = require("better-sqlite3");
-// const db = sql("main.db");
-// db.pragma('foreign_keys = ON');
-
-// const rows = db.prepare(`SELECT * FROM listings`).all();
-// console.log(rows); 
-
-// db.close();
-// console.log("Database connection closed.");
