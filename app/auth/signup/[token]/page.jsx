1b71b5a7f8550805c2ea9c128ead23d6c5819148
esayadmohamed <esayadmohamed@gmail.com>
2025-05-03 14:06:39 +0100

    Made changes to my app

--- app/auth/signup/[token]/page.jsx
+++ app/auth/signup/[token]/page.jsx
@@ -6,7 +6,6 @@ import { redirect } from 'next/navigation';
 
 import Header from '@/_com/header/Header';
 import Footer from '@/_com/footer/Footer';
-
 import AuthToken from "@/_com/auth/token";
 
 export default async function AuthVerify ({params}){
@@ -14,9 +13,9 @@ export default async function AuthVerify ({params}){
     const token = await params;
     const token_code = token.token;
 
-    const baseurl = getBaseUrl();
-    const res = await fetch(`${baseurl}/api/token?token_value=${token_code}`, { cache: 'no-store' });
-    const data = await res.json();
+    // const baseurl = getBaseUrl();
+    // const res = await fetch(`${baseurl}/api/token?token_value=${token_code}`, { cache: 'no-store' });
+    // const data = await res.json();
 
     const session = await getSessionData();
     
