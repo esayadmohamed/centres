d34746255cc941ef6b6e80dd1a90fa14ab906f3e
esayadmohamed <esayadmohamed@gmail.com>
2025-05-03 14:13:19 +0100

    Made changes to my app

---
+++ app/auth/reset/[token]/page.jsx
@@ -0,0 +1,25 @@
+import { redirect } from 'next/navigation'; 
+import { getSessionData } from '@/_lib/utils/session';
+
+import Header from '@/_com/header/Header';
+import Footer from "@/_com/footer/Footer";
+import ResetPassword from '@/_com/auth/reset';
+
+export default async function ResetVerify ({params}){
+    
+    const token = await params;
+    const token_code = token?.token;
+    
+    const session = await getSessionData();
+
+    if(session) {redirect('/');}
+    return(
+        <div className="content">
+            <Header session={false}/>
+
+            <ResetPassword reset_token={token_code}/>
+
+            <Footer />
+        </div>
+    )
+}
